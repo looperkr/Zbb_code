e74464cb4ba30f1b5c1f29096487c4e540855e21
KLooper <elfofdoriath@gmail.com>
2017-06-06 15:23:58 -0400

    roofit template fit plots use ROOT, correct for bin normalization from RooFit

--- roofit_template.C
+++ roofit_template.C
@@ -4,11 +4,10 @@
 
 void roofit_template(){
   using namespace RooFit;
-  gROOT->LoadMacro("AtlasUtils.C");
-  gROOT->LoadMacro("AtlasLabels.C");
 
   bool isHadMatch = true;
-  //bottom_jets_hmatch.root
+  bool isStack = false;
+  bool isPrefit = true;
 
   string hist_name = "zmumu_sum";
   string hist_dir = "/n/atlas02/user_codes/looper.6/Vbb/analysis_code/MC_histograms_root/";
@@ -42,15 +41,16 @@ void roofit_template(){
   TH1D *hdata = (TH1D*)f_data->Get(data_hist_name.c_str());
 
 
-  Int_t Ndata = hdata->Integral();
   Int_t Nlight = hlight->Integral();
   Int_t Ncharm = hcharm->Integral();
   Int_t Nbottom = hbottom->Integral();
+  Int_t Ndata = hdata->Integral();
 
-  RooRealVar x("x","x",-20,20);
-  //  RooRealVar fsig("fsig","signal fraction",0.,1.);
-  RooRealVar frbottom("frbottom","bottom fraction",0.,1.);
-  RooRealVar frcharm("frcharm","charm fraction",0.,1.);
+  RooRealVar x("x","MV1c weight",0.,1.);
+  
+  RooRealVar frbottom("bjet_fraction","bottom fraction",0.,1.);
+  RooRealVar frcharm("cjet_fraction","charm fraction",0.,1.);
+  RooRealVar frlight("ljet_fraction","light fraction",0.,1.);
 
   RooDataHist data("data","dataset with x",x,hdata);
   RooDataHist bjetMC("bjetMC","bjetMC with x",x,hbottom);
@@ -60,26 +60,96 @@ void roofit_template(){
   RooHistPdf bjetTemplate("bjetTemplate","bjetTemplate",x,bjetMC);
   RooHistPdf cjetTemplate("cjetTemplate","cjetTemplate",x,cjetMC);
   RooHistPdf ljetTemplate("ljetTemplate","ljetTemplate",x,ljetMC);
-
-  cout << "test" << endl;
-
   RooAddPdf template_model("model","model",RooArgList(bjetTemplate,cjetTemplate,ljetTemplate),RooArgList(frbottom,frcharm));
 
-  template_model.fitTo(data);
-
   RooPlot* xframe = x.frame();
-  data.plotOn(xframe,Name("data"),DataError(RooAbsData::SumW2));
-  template_model.plotOn(xframe,Name("model"));
-  template_model.plotOn(xframe,Components(bjetTemplate),LineColor(kGreen),LineStyle(kDashed),Name("bjets"));
-  template_model.plotOn(xframe,Components(cjetTemplate),LineColor(kRed),LineStyle(kDashed),Name("cjets"));
-  template_model.plotOn(xframe,Components(ljetTemplate),LineColor(kYellow),LineStyle(kDashed),Name("ljets"));
-  template_model.paramOn(xframe);
-  xframe->Draw();
+  TCanvas *c1 = new TCanvas("c1","c1",1200,800);
 
-  THStack *sum_stack = new THStack("stack","stack");
+  RooFitResult *fitres = template_model.fitTo(data,Save(kTRUE),SumW2Error(kTRUE),PrintEvalErrors(-1));
+  double b_result = frbottom.getVal();
+  double c_result = frcharm.getVal();
+  double l_result = 1-b_result-c_result;
+
+  /*
+    xframe->SetMaximum(2000);
+    xframe->SetMinimum(0);
+    data.plotOn(xframe,Name("data"),DataError(RooAbsData::SumW2));
+    template_model.plotOn(xframe,Name("model"),LineColor(kBlue));
+    template_model.plotOn(xframe,Components(bjetTemplate),LineColor(kGreen),LineStyle(kDashed),Name("bjets"));
+    template_model.plotOn(xframe,Components(cjetTemplate),LineColor(kRed),LineStyle(kDashed),Name("cjets"));
+    template_model.plotOn(xframe,Components(ljetTemplate),LineColor(kYellow),LineStyle(kDashed),Name("ljets"));
+    template_model.paramOn(xframe);
+  */
+
+  if(!isPrefit){
+    hbottom->Scale(Ndata*b_result/Nbottom);
+    hcharm->Scale(Ndata*c_result/Ncharm);
+    hlight->Scale(Ndata*l_result/Nlight);
+  }
+  TH1F *final_sum = (TH1F*)hbottom->Clone();
+  final_sum->Add(hcharm);
+  final_sum->Add(hlight);
+
+  hdata->SetLineColor(kBlack);
+  final_sum->SetLineColor(kBlue);
+
+  TLegend *leg = new TLegend(0.6,0.64,0.95,0.80);
+  leg->SetTextSize(0.03);
+
+  c1->SetLogy();
 
 
 
+  if(!isStack){
+    hbottom->SetLineColor(kGreen);
+    hcharm->SetLineColor(kRed);
+    hlight->SetLineColor(kYellow);
+   
+    hbottom->SetLineStyle(kDashed);
+    hcharm->SetLineStyle(kDashed);
+    hlight->SetLineStyle(kDashed);
+    
+    hdata->Draw("p");
+    final_sum->Draw("hist same");
+    hbottom->Draw("hist same");
+    hcharm->Draw("hist same");
+    hlight->Draw("hist same");
 
+    leg->AddEntry(hdata,"Data","lp");
+    leg->AddEntry(final_sum,"fit result","l");
+    leg->AddEntry(hbottom,"bottom","l");
+    leg->AddEntry(hcharm,"charm","l");
+    leg->AddEntry(hlight,"light","l");
+
+    leg->Draw();
+
+  }
+  else{
+    hbottom->SetFillColor(kGreen);
+    hcharm->SetFillColor(kRed);
+    hlight->SetFillColor(kYellow);
+
+    THStack *mc_stack =  new THStack("stack","stack");
+    mc_stack->Add(hbottom);
+    mc_stack->Add(hcharm);
+    mc_stack->Add(hlight);
+
+    mc_stack->SetMinimum(5000);
+
+    mc_stack->Draw("hist");
+    hdata->Draw("p same");
+
+
+    leg->AddEntry(hdata,"Data","lp");
+    leg->AddEntry(hbottom,"bottom","f");
+    leg->AddEntry(hcharm,"charm","f");
+    leg->AddEntry(hlight,"light","f");
+
+    leg->Draw();
+
+    cout << hdata << endl;
+
+
+  }
 
 }

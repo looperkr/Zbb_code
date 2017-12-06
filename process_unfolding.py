4593d7c888ff7784c18208cc60d1e42876ce84fb
KLooper <elfofdoriath@gmail.com>
2017-12-06 18:02:25 -0500

    Made jet distributions easier to add

--- process_unfolding.py
+++ process_unfolding.py
@@ -3,9 +3,12 @@ import os,sys
 
 #Dictionary stores list with ["distributionname",min,max,binning]
 
-distribution_dict = {"Z_mass":["Mll",70,110,20],"Z_pt":["Pt",0,1000,20],"Z_y":["Y",-3.5,3.5,20]}
+distribution_dict = {"Z_mass":"Mll","Z_mass_MET":"Mll_MET","Z_pt":"Pt","Z_y":"Y","n_jets_tightmet":"nJetsInBTagAccept_ex", "leadjet_pt_tightmet":"bTagAcceptance_leadJet_Pt"}
+truth_name = {"Z_mass":"Z_mass","Z_mass_MET":"Z_mass","Z_pt":"Z_pt", "Z_pt_MET":"Z_pt", "Z_y":"Z_y", "Z_y_MET":"Z_y",
+              "n_jets_tightmet":"n_jets","leadjet_pt_tightmet":"leadjet_pt"}
 
 distribution = sys.argv[1]
+truth_distribution = truth_name[distribution]
 
 TH1.AddDirectory(0)
 
@@ -18,18 +21,17 @@ uf_bkg_samples = ["Ztautau","WmunuL_Alpgen","WmunuCC_Alpgen","WmunuC_Alpgen","Wm
 uf_h_name_sig = "Z_Alpgen"
 uf_h_name_bkg = ["Ztautau","W_Alpgen","W_Alpgen","W_Alpgen","W_Alpgen","ttbar","stopt","stops","stopWt","WW","WZ","ZZ"]
 uf_channel = "_Mu_Z"
-uf_min = uf_distribution[1]
-uf_max = uf_distribution[2]
-uf_binning = uf_distribution[3]
 
 uf_hist_list = ["","_truth_dressed","_match","_unmatch","_migration"]
+
+
 
 gROOT.ProcessLine(".L analysis_plots.C+")
 
 for hist in uf_hist_list:
     var_2_plot = distribution
-    if distribution == "Z_mass" and hist != "_truth_dressed":
-        var_2_plot += "_MET"
+    if hist == "_truth_dressed":
+        var_2_plot = truth_distribution
     var_2_plot += hist
     cmd = "analysis_plots(\"" + var_2_plot + "\",true,true,false)"
     print cmd
@@ -39,19 +41,20 @@ for hist in uf_hist_list:
 hist_arr = []
 plot_file = distribution
 for hist in uf_hist_list:
-    if distribution == "Z_mass" and hist != "_truth_dressed":
-        plot_file = distribution + "_MET"
+    var_f_name = ""
+    if hist == "_truth_dressed":
+        var_f_name = "unfolding_preprocessed/"+ truth_distribution + hist + ".root"
     else:
-        plot_file = distribution
-    var_f_name = "unfolding_preprocessed/"+ plot_file + hist + ".root"
+        var_f_name = "unfolding_preprocessed/"+ distribution + hist + ".root"
     f = TFile(var_f_name,"READ")
     var_arr = []
     uf_h_name = uf_h_name_sig
     if hist == "_truth_dressed":
         uf_h_name += "_dressedMu_dressedZ_truth_"
-        uf_h_name += uf_distribution[0]
+        uf_h_name += uf_distribution
     else:
-        uf_h_name = uf_h_name + uf_channel + hist + "_" + uf_distribution[0]
+        uf_h_name = uf_h_name + uf_channel + hist + "_"
+        uf_h_name += uf_distribution
     for sample in sig_samples:
         sample_hist = sample + "_sum"
         h = f.Get(sample_hist)
@@ -60,6 +63,7 @@ for hist in uf_hist_list:
         h.SetName(uf_h_name)
         var_arr.append(h)
     hist_arr.append(var_arr)
+    print uf_h_name
 
 for j in range(0,len(sig_samples)):
     uf_fname = "unfolding_inputs/hist-"+uf_sig_samples[j] + ".root"
@@ -69,25 +73,25 @@ for j in range(0,len(sig_samples)):
         uf_h.Write()
     uf_f.Close()
 
-var_2_plot = distribution
-if distribution == "Z_mass":
-    var_2_plot+="_MET"
-bkg_var_f_name = "unfolding_preprocessed/"+ var_2_plot +".root"
+
+bkg_var_f_name = "unfolding_preprocessed/"+ distribution +".root"
 f_reco = TFile(bkg_var_f_name,"READ")
 for k in range(0,len(bkg_samples)):
     sample_hist = bkg_samples[k] + "_sum"
     h = f_reco.Get(sample_hist)
     if not h:
         print "Could not find histogram " + sample_hist + "in file " + bkg_var_f_name
-    uf_h_name = uf_h_name_bkg[k] + "_" +  uf_channel + "_" + uf_distribution[0]
+    uf_h_name = uf_h_name_bkg[k] + "_" +  uf_channel + "_"
+    uf_h_name += uf_distribution
     h.SetName(uf_h_name)
+    print uf_h_name
     uf_fname = "unfolding_inputs/hist-"+uf_bkg_samples[k]+".root"
     uf_f = TFile(uf_fname,"UPDATE")
     h.Write()
     uf_f.Close()
 
 h_data = f_reco.Get("data")
-uf_h_name_data = "data"+uf_channel+"_"+ uf_distribution[0]
+uf_h_name_data = "data"+uf_channel+"_"+ uf_distribution
 h_data.SetName(uf_h_name_data)
 uf_fname_data = "unfolding_inputs/hist-data.root"
 uf_f_data = TFile(uf_fname_data,"UPDATE")
